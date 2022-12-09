27ac8c4e21fe5b4db2fb0e003971552c2e07c829
curlerhaus <101598928+curlerhaus@users.noreply.github.com>
2022-12-09 17:26:00 -0600

    create settings page

--- webblog/src/App.js
+++ webblog/src/App.js
@@ -1,7 +1,8 @@
 // import Home from "./pages/homepage/Home";
 import TopBar from "./components/topbar/TopBar";
+import Settings from "./pages/settings/Settings";
 // import Single from "./pages/single/Single";
-import Write from "./pages/write/Write";
+// import Write from "./pages/write/Write";
 
 function App() {
   return (
@@ -9,7 +10,8 @@ function App() {
       <TopBar />
       {/* <Home /> */}
       {/* <Single /> */}
-      <Write />
+      {/* <Write /> */}
+      <Settings />
     </>
   );
 }
