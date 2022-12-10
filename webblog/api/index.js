d08465861494832de52da1a5a8c13860e4d5ae1a
curlerhaus <101598928+curlerhaus@users.noreply.github.com>
2022-12-10 14:12:59 -0600

    categories route

--- webblog/api/index.js
+++ webblog/api/index.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
 const port = 3003;
 dotenv.config();
 app.use(express.json());
@@ -21,6 +22,7 @@ mongoose
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryRoute);
 
 app.listen(port, () => {
   console.log("I am alive at port: ", port);
