3fcba2f156a2a4dad48771d2f7abc7ff5c8636ae
curlerhaus <101598928+curlerhaus@users.noreply.github.com>
2022-12-10 13:15:46 -0600

    added user routes

--- webblog/api/index.js
+++ webblog/api/index.js
@@ -3,6 +3,7 @@ const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
 const port = 3003;
 dotenv.config();
 app.use(express.json());
@@ -17,6 +18,7 @@ mongoose
   .catch((err) => console.log(err));
 
 app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
 
 app.listen(port, () => {
   console.log("I am alive at port: ", port);
