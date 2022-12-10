dccd7e40528182ee799a6eb1194a04cfd8473bd8
curlerhaus <101598928+curlerhaus@users.noreply.github.com>
2022-12-10 14:29:42 -0600

    added file upload feature

--- webblog/api/index.js
+++ webblog/api/index.js
@@ -6,6 +6,7 @@ const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
 const categoryRoute = require("./routes/categories");
+const multer = require("multer");
 const port = 3003;
 dotenv.config();
 app.use(express.json());
@@ -18,6 +19,20 @@ mongoose
   })
   .then(console.log("Connected to MongoDB."))
   .catch((err) => console.log(err));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, "hello.jpeg");
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded.");
+});
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
