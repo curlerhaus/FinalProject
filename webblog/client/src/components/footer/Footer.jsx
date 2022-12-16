8dc8ac03bf8fd572ec989d5d54c7fcfbe1018f34
curlerhaus <101598928+curlerhaus@users.noreply.github.com>
2022-12-16 16:05:08 -0600

    made updates

--- webblog/client/src/components/footer/Footer.jsx
+++ webblog/client/src/components/footer/Footer.jsx
@@ -2,5 +2,11 @@ import React from "react";
 import "./footer.css";
 
 export default function Footer() {
-  return <div className="footer"></div>;
+  return (
+    <div className="footer">
+      <small>
+        &copy; Copyright {new Date().getFullYear()}, Joel Dietz, Inc
+      </small>
+    </div>
+  );
 }
--- webblog/client/src/components/topbar/TopBar.jsx
+++ webblog/client/src/components/topbar/TopBar.jsx
@@ -1,5 +1,5 @@
-import "./topbar.css"
-import {Link} from "react-router-dom"
+import "./topbar.css";
+import { Link } from "react-router-dom";
 import { Context } from "../../context/Context";
 import { useContext } from "react";
 
@@ -7,46 +7,67 @@ export default function TopBar() {
   const { user, dispatch } = useContext(Context);
 
   const handleLogout = () => {
-    dispatch({type:"LOGOUT"})
-  }
-
+    dispatch({ type: "LOGOUT" });
+  };
 
   return (
-    <div className='topbar'>
-        <div className="topLeft">
-          {
-            user ? (
-              <div />
-            ): (
-              <>
-                <ul className="topList">
-                  <li className="topListItem">
-                  <Link className="link" to='/login'>LOGIN</Link>
-                  </li>
-                  <li>
-                  <Link className="link" to='/register'>REGISTER</Link>
-                  </li>
-                </ul>
-              </>
-            )
-          }
-        </div>
-        <div className="topCenter">
+    <div className="topbar">
+      <div className="topLeft">
+        {user ? (
+          <div>
+            <p>Welcome {user.username}</p>
+          </div>
+        ) : (
+          <>
             <ul className="topList">
-                <li className="topListItem"><Link className="link" to="/">HOME</Link></li>
-                <li className="topListItem"><Link className="link" to="/about">ABOUT</Link></li>
-                <li className="topListItem"><Link className="link" to="/contact">CONTACT</Link></li>
-                <li className="topListItem"><Link className="link" to="/write">WRITE</Link></li>
-                <li className='topListItem' onClick={handleLogout}>{user && "LOGOUT"}</li>
+              <li className="topListItem">
+                <Link className="link" to="/login">
+                  LOGIN
+                </Link>
+              </li>
+              <li>
+                <Link className="link" to="/register">
+                  REGISTER
+                </Link>
+              </li>
             </ul>
-        </div>
-        <div className="topRight">
+          </>
+        )}
+      </div>
+      <div className="topCenter">
+        <ul className="topList">
+          <li className="topListItem">
+            <Link className="link" to="/">
+              HOME
+            </Link>
+          </li>
+          <li className="topListItem">
+            <Link className="link" to="/about">
+              ABOUT
+            </Link>
+          </li>
+          <li className="topListItem">
+            <Link className="link" to="/contact">
+              CONTACT
+            </Link>
+          </li>
+          <li className="topListItem">
+            <Link className="link" to="/write">
+              WRITE
+            </Link>
+          </li>
+          <li className="topListItem" onClick={handleLogout}>
+            {user && "LOGOUT"}
+          </li>
+        </ul>
+      </div>
+      <div className="topRight">
         <i className="topIcon fa-brands fa-square-facebook"></i>
         <i className="topIcon fa-brands fa-square-twitter"></i>
         <i className="topIcon fa-brands fa-instagram"></i>
         <i className="topIcon fa-brands fa-square-pinterest"></i>
         <i className="topSearchIcon fa-solid fa-magnifying-glass"></i>
-        </div>
+      </div>
     </div>
-  )
+  );
 }
